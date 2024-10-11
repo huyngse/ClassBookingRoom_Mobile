663615783e5ef6df12b34171a382be9122421295
vuvuvo0303 <levanhoangvu2003@gmail.com>
2024-10-11 23:01:29 +0700

    modify login  page

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,43 +1,67 @@
-import { View, Text } from "react-native";
+import { View, Text, Image, TouchableOpacity } from "react-native";
 import React, { useState } from "react";
 import { Button, TextInput } from "react-native-paper";
 import { Link, router } from "expo-router";
-
 const SignIn = () => {
   const [text, setText] = useState("");
   const [password, setPassword] = useState("");
+
   const login = () => {
-    router.replace('/(root)/(tabs)/home');
-  }
+    router.replace("/(root)/(tabs)/home");
+  };
+
   return (
-    <View className="px-5 h-screen">
-      <View className="justify-center gap-3 flex-1">
-        <Text className="text-center text-purple-700 text-5xl mb-5">Login</Text>
-        <TextInput
-          label="Email"
-          value={text}
-          onChangeText={(text) => setText(text)}
-        />
+    <View className="px-5 h-screen bg-white">
+      <View className="items-center mt-10">
+        <Image source={require("../../assets/images/logo2-.png")} className="w-48 h-48" resizeMode="contain" />
+      </View>
+
+      <View className="justify-center gap-4 flex-1">
+        <View className="flex items-center pb-6">
+          <Text className="text-center text-orange-500 text-4xl font-bold ">FPT Booking Room</Text>
+          <Text>For Staff</Text>
+        </View>
+
+        <TextInput label="Email" value={text} onChangeText={(text) => setText(text)} mode="outlined" className="mb-3" />
         <TextInput
           label="Password"
           value={password}
           secureTextEntry={true}
           onChangeText={(password) => setPassword(password)}
+          mode="outlined"
+          className="mb-6"
         />
-        <Button
-          mode="contained"
-          onPress={login}
-          className="p-1"
-        >
-          Continue
+        <Button mode="contained" onPress={login} className="py-1 rounded-lg" buttonColor="orange">
+          <Text className="text-black">Login</Text>
         </Button>
-        <View className="flex-row justify-end">
+
+        {/* <View className="flex-row justify-end mt-3">
           <Link href={"/(auth)/forgot-password"}>
-            <Text className="text-end text-purple-700 text-xs">Forgot password?</Text>
+            <Text className="text-end text-purple-700 text-sm">
+              Forgot password?
+            </Text>
           </Link>
+        </View> */}
+
+        <View className="flex-row items-center my-5">
+          <View className="flex-1 h-[1px] bg-gray-300" />
+          <Text className="mx-3 text-gray-600">Or</Text>
+          <View className="flex-1 h-[1px] bg-gray-300" />
+        </View>
+
+        <View className="items-center mt-8">
+          <TouchableOpacity
+            onPress={() => {}}
+            className="w-64 flex-row items-center justify-center py-3 bg-white rounded-lg border border-gray-300"
+          >
+            <Image source={require("../../assets/images/google.webp")} className="w-6 h-6 mr-2" resizeMode="contain" />
+            <Text className="text-black text-lg">Login with Google</Text>
+          </TouchableOpacity>
         </View>
       </View>
-      <Text className="text-center text-xs">© All right reversed</Text>
+      <View></View>
+
+      <Text className="text-center text-xs text-gray-600 mt-5">© FPT Booking Room</Text>
     </View>
   );
 };
